48b53b80a87ded091535a0c570eff5dcb2604b7a
Afr Schoe <58883403+q9f@users.noreply.github.com>
2024-12-19 15:43:14 +0100

    examples: update for phase 6

--- examples/distance.rb
+++ examples/distance.rb
@@ -1,5 +1,10 @@
 #!/usr/bin/env ruby
 
+
+# USAGE
+# ruby examples/distance.rb "D:S299" "Y:1SII"
+# "D:S299 --> Y:1SII: 885.623 ly"
+
 # use the local version of the code instead of a globally installed gem
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 
@@ -10,34 +15,23 @@ include Frontier
 
 if __FILE__ == $0
 
-  # Star system IDs are offset by 30 million
-  STAR_ID_OFFSET = 30_000_000
-
-  # Amount of star systems in phase 5
-  STAR_COUNT = 20_230
-  LAST_STAR_ID = STAR_ID_OFFSET + STAR_COUNT
-
   # Make the universe available in these varaibles
   ALL_STARS = {}
 
   # Read star data (locally or from API)
-  stars_phase5 = File.read "./spec/fixtures/stars_phase5.json"
-  stars_phase5 = JSON.parse stars_phase5
-  mapping_closed_alpha = File.read "./spec/fixtures/mapping_closed_alpha.json"
-  mapping_closed_alpha = JSON.parse mapping_closed_alpha
+  stars_closed_alpha = File.read "./spec/fixtures/stars_closed_alpha.json"
+  stars_closed_alpha = JSON.parse stars_closed_alpha
 
   # Create all star system objects with coordinates
-  c_id = STAR_ID_OFFSET + 1
-  prog = 0
-  while c_id < LAST_STAR_ID
-    id = c_id.to_s
-    location = Coords.new stars_phase5[id]["location"]["x"], stars_phase5[id]["location"]["y"], stars_phase5[id]["location"]["z"]
-    star = Star.new id, mapping_closed_alpha[id], location
+  stars_closed_alpha.each do |star|
+    key = star[0]
+    value = star[1]
+    location = Coords.new value["location"]["x"], value["location"]["y"], value["location"]["z"]
+    star = Star.new key, value["solarSystemName"], location
     ALL_STARS[star.name] = star
-    c_id += 1
   end
 
-  # Direct distance between "D:S299" to "Y:1SII"
-  direct_distance = ALL_STARS["D:S299"].distance_ly(ALL_STARS["Y:1SII"])
-  pp direct_distance
+  # Direct distance between argument 1 and 2
+  direct_distance = ALL_STARS[ARGV[0]].distance_ly(ALL_STARS[ARGV[1]])
+  pp "#{ARGV[0]} --> #{ARGV[1]}: #{'%.3f' % direct_distance} ly"
 end
